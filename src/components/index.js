cec5c88cd6b09d411bd8d8fbd3ac19c07e884942
Anonymous-alpha-linux <tinhntgcd18753@fpt.edu.vn>
2022-02-09 15:06:32 +0700

    save current develop

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export { default as ContainerComponent } from './container';
 export { default as Form } from './form';
 export { default as Button } from './button';
 export { default as Slider } from './slider';
-export { default as Table } from './table';
+export { default as Table } from './table';add
