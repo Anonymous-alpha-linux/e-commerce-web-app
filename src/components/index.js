ede3942c25f40bcb20bcedc29c3b77f8e172480c
OS <OS@DESKTOP-NLC0EVM>
2022-02-19 20:49:49 +0700

    save khanh branch

--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,7 @@ export { default as Table } from './table';
 export { default as Icon } from './icon';
 export { default as Text } from './text';
 export {default as Preview} from './preview';
-export { default as MessageBox} from './messagebox'
+export { default as MessageBox} from './messagebox';
 export {default as Message} from './message';
-export { default as MessageBox } from "./messagebox";
 export { default as List } from "./list";
 export { default as Loader } from "./loading";
